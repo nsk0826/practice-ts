1cef95d43a960f6a187be30d0cc1cffcf03a1484
nsk0826 <nsk.20160401@gmail.com>
2022-05-06 00:38:28 +0900

    add code

--- src/sample1.ts
+++ src/sample1.ts
@@ -1,1 +1,12 @@
 import { createInterface } from 'readline';
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('名前を入力してください：',(name) => {
+  const displayName = name || "名無し";
+  console.log(`こんにちは　${displayName}さん`)
+  rl.close();
+})
