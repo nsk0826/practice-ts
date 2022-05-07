33ce5a2cf785b0b1928afeda2262fd1c5931ddd7
nsk0826 <nsk.20160401@gmail.com>
2022-05-07 23:05:51 +0900

    add fizzbuzz2

---
+++ src/fizzBuzz2.ts
@@ -0,0 +1,13 @@
+let result: string = "";
+for (let i: number = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    result += ",FizzBuzz " + i;
+  } else if (i % 5 === 0) {
+    result += ",Buzz " + i;
+  } else if (i % 3 === 0) {
+    result += ",Fizz " + i;
+  } else {
+    result += "," + i;
+  }
+}
+console.log(result)
