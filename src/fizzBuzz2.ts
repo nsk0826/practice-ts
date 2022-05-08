2ad7f62471da5fc6621d32df983b58ed2a4330f5
nsk0826 <nsk.20160401@gmail.com>
2022-05-08 22:22:27 +0900

    fix

--- src/fizzBuzz2.ts
+++ src/fizzBuzz2.ts
@@ -4,13 +4,13 @@ for (let i: number = 1; i <= 100; i++) {
     result += " ";
   }
   if (i % 3 === 0 && i % 5 === 0) {
-    result += ",FizzBuzz " + i;
+    result += "FizzBuzz " + i;
   } else if (i % 5 === 0) {
-    result += ",Buzz " + i;
+    result += "Buzz " + i;
   } else if (i % 3 === 0) {
-    result += ",Fizz " + i;
+    result += "Fizz " + i;
   } else {
-    result += "," + i;
+    result += i;
   }
 }
 console.log(result);
