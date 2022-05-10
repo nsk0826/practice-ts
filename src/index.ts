adfeb685f1f36d35f4d828d13e3f4f2e64776362
nsk0826 <nsk.20160401@gmail.com>
2022-05-11 00:11:21 +0900

    g flg の追加

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,10 @@ const greeting = "Hello, ";
 const target = greeting + greeting;
 console.log(target);
 
-const r = /ab+c/
+const r = /ab+c/i
 
 console.log( r.test("abc"))
 console.log(r.test("aaabc"))
 console.log(r.test("a"))
 console.log(r.test("ありがとう"))
+console.log(r.test("ABC"))
