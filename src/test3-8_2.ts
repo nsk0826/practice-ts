87b9f34cd584ddbd9684ef5caf8340fe35a90a9b
nsk0826 <nsk.20160401@gmail.com>
2022-05-12 00:25:32 +0900

    add 別バージョン

---
+++ src/test3-8_2.ts
@@ -0,0 +1,41 @@
+type Human = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+};
+
+const datas: string = `
+uhyo,26,1
+john Smith,17,0
+Mary Sue,14,1
+`;
+
+const humans: Human[] = [];
+
+const lines = datas.split("\n");
+
+for (const line of lines) {
+  if (line === "") {
+    continue;
+  }
+  const [name, ageString, premiumUserString] = line.split(",");
+
+  const age = Number(ageString);
+  const premiumUser = premiumUserString === "1";
+
+  humans.push({
+    name,
+    age,
+    premiumUser,
+  });
+}
+
+for (const human of humans) {
+  if (human.premiumUser) {
+    console.log(`${human.name} (${human.age})はプレミアムユーザーです。`);
+  } else {
+    console.log(
+      `${human.name} (${human.age})はプレミアムユーザーではありません。`
+    );
+  }
+}
