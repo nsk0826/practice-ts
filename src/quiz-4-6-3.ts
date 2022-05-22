9ed399fcb73f72c8e4e865ed337d6130d6075648
nsk0826 <nsk.20160401@gmail.com>
2022-05-23 00:58:35 +0900

    quizの実装

--- src/quiz-4-6-3.ts
+++ src/quiz-4-6-3.ts
@@ -1,13 +1,19 @@
-function map(array: number[], callback: ):number[] {
+const map = <T>(array: number[], callback: (x: number) => T): T[] => {
+  const result: T[] = [];
+  array.map((v) => {
+    return result.push(callback(v));
+  });
+  return result;
+};
 
-}
+const numbers = [1, 1, 2, 3, 5, 8, 13];
 
-const numbers = [1,1,2,3,5,8,13];
+const resultNumbers = map(numbers, (x: number) => x * 10);
 
-const resultNumbers = map(numbers, (x:number) => x * 10);
+const numbers2 = [1, -3, -2, 8, 0];
+
+const resultBoolean = map(numbers2, (x: number) => x >= 0);
 
 console.log(resultNumbers);
 
-
-
-
+console.log(resultBoolean)
