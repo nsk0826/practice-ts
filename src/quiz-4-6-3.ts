ef83cc47e5d4376ae3e12ac0fcaa9300d12c1ab9
nsk0826 <nsk.20160401@gmail.com>
2022-05-23 01:00:01 +0900

    配列に値を追加

--- src/quiz-4-6-3.ts
+++ src/quiz-4-6-3.ts
@@ -10,7 +10,7 @@ const numbers = [1, 1, 2, 3, 5, 8, 13];
 
 const resultNumbers = map(numbers, (x: number) => x * 10);
 
-const numbers2 = [1, -3, -2, 8, 0];
+const numbers2 = [1, -3, -2, 8, 0, -1];
 
 const resultBoolean = map(numbers2, (x: number) => x >= 0);
 
