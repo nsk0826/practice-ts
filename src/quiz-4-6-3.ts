a2d67b6f3ad2b899cb2fcf5c64491dfa37362dd8
nsk0826 <nsk.20160401@gmail.com>
2022-05-23 00:42:36 +0900

    実装前

---
+++ src/quiz-4-6-3.ts
@@ -0,0 +1,13 @@
+function map(array: number[], callback: ):number[] {
+
+}
+
+const numbers = [1,1,2,3,5,8,13];
+
+const resultNumbers = map(numbers, (x:number) => x * 10);
+
+console.log(resultNumbers);
+
+
+
+
