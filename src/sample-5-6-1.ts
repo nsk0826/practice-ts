5d765bc4124677b899a7512b1e7e18c952a9b3d5
nsk0826 <nsk.20160401@gmail.com>
2022-06-02 23:30:27 +0900

    クロージャでの実装

--- src/sample-5-6-1.ts
+++ src/sample-5-6-1.ts
@@ -1,21 +1,28 @@
-class UserClass  {
-  constructor(name:string, age:number){
-
-    if(name === ""){
+class UserClass {
+  constructor(name: string, age: number) {
+    if (name === "") {
       throw new Error("名前は空にできません！");
     }
-    this.name = name,
-    this.age = age
+    (this.name = name), (this.age = age);
   }
   name: string;
   age: number;
 
-  getMessage(message: string){
+  getMessage(message: string) {
     return `${this.name}(${this.age}) 「${message}」`;
   }
-};
-
+}
 
 const uhyo = new UserClass("uhyo", 26);
 
 console.log(uhyo.getMessage("こんにちは"));
+
+const createUser = (name: string, age: number) => {
+  return (message: string) => {
+    return `${name}(${age})「${message}」`;
+  };
+};
+
+const getMessage = createUser("山田", 60);
+
+console.log(getMessage("こんばんは"));
