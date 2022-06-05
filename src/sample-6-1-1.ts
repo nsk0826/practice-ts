cdde9bf972e4de4924c463cb8957ebf241050613
nsk0826 <nsk.20160401@gmail.com>
2022-06-06 00:03:06 +0900

    ユニオン型

--- src/sample-6-1-1.ts
+++ src/sample-6-1-1.ts
@@ -1,4 +1,17 @@
 type Animal = {
-  species: string
-}
+  species: string;
+};
 
+type Human2 = {
+  name: string;
+};
+
+type User2 = Animal | Human2;
+
+const tama: User2 = {
+  species: "silver cat",
+};
+
+const fooMan: User2 = {
+  name: "foo",
+};
