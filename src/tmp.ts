e5d7bda14345b9959acba6280f0cd586a361acdb
nsk0826 <nsk.20160401@gmail.com>
2022-06-08 00:42:35 +0900

    TYPEに関数の追加

--- src/tmp.ts
+++ src/tmp.ts
@@ -1,6 +1,15 @@
 type sampleF = {
   name: string;
   age: number;
+  func: (n: number) => number;
 };
 
 type F = (args: number) => number;
+
+const ff :sampleF = {
+  name: "taro",
+  age: 10,
+  func: (n: number) => {
+    return n + 1; 
+  }
+}
