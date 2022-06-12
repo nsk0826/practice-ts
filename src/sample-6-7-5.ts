4a88a1eb979f5a7b480376fe52190d9b127fc2d4
nsk0826 <nsk.20160401@gmail.com>
2022-06-12 23:04:12 +0900

    fix

--- src/sample-6-7-5.ts
+++ src/sample-6-7-5.ts
@@ -1,7 +1,9 @@
 type RestArgs<M> = M extends "string"
-  ? ["string", "string"]
+  ? [string, string]
   : [number, number, number];
 
 const func = <M extends "string" | "number">(mode: M, ...args: RestArgs<M>) => {
   console.log(mode, ...args);
 };
+
+func("string","aaaa","bbbb");
