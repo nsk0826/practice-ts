703645797726b763aa4ca28b3c88ed46cafd7b32
nsk0826 <nsk.20160401@gmail.com>
2022-06-13 00:03:24 +0900

    add isSome

--- src/quiz-6-8.ts
+++ src/quiz-6-8.ts
@@ -8,9 +8,13 @@ type Option<T> =
     };
 
 const showNumberIfExists = (obj: Option<number>): void => {
-  if (obj.tag === "same") {
+  if (isSome(obj)) {
     console.log(obj.value);
   }
+};
+
+const isSome = <T>(obj: Option<T>): obj is { tag: "same"; value: T } => {
+  return obj.tag === "same";
 };
 
 const four: Option<number> = {
@@ -24,12 +28,3 @@ const nothing: Option<number> = {
 
 showNumberIfExists(four);
 showNumberIfExists(nothing);
-
-type Option2<T> =
-  | {
-      tag: "ame";
-      val: T;
-    }
-  | {
-      tag: "none";
-    };
