20fd60b069cbe18b48fa7fb5eaba9182399dc50a
nsk0826 <nsk.20160401@gmail.com>
2022-06-12 23:04:47 +0900

    add number

--- src/sample-6-7-5.ts
+++ src/sample-6-7-5.ts
@@ -6,4 +6,6 @@ const func = <M extends "string" | "number">(mode: M, ...args: RestArgs<M>) => {
   console.log(mode, ...args);
 };
 
-func("string","aaaa","bbbb");
+func("string", "aaaa", "bbbb");
+
+func("number", 123, 456, 789);
