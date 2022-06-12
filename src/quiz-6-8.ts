38d4d8df92ebc085adfb0d17c6253d791fdefac9
nsk0826 <nsk.20160401@gmail.com>
2022-06-12 23:52:26 +0900

    add quiz

---
+++ src/quiz-6-8.ts
@@ -0,0 +1,26 @@
+type Option<T> =
+  | {
+      tag: "same";
+      value: T;
+    }
+  | {
+      tag: "none";
+    };
+
+const showNumberIfExists = (obj: Option<number>): void => {
+  if (obj.tag === "same") {
+    console.log(obj.value);
+  }
+};
+
+const four: Option<number> = {
+  tag: "same",
+  value: 4,
+};
+
+const nothing: Option<number> = {
+  tag: "none",
+};
+
+showNumberIfExists(4);
+showNumberIfExists();
