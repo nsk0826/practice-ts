6c6f785a19a7c999c52a411485a5b4a9670a952f
nsk0826 <nsk.20160401@gmail.com>
2022-06-20 00:41:35 +0900

    add quiz

--- src/quiz-7-4.ts
+++ src/quiz-7-4.ts
@@ -5,4 +5,17 @@ const txt = readFileSync("sample.txt", { encoding : "utf8"});
 console.log(txt)
 
 let count = 0
-let nextIndex = 0
+let currentIndex = 0
+
+while(true){
+  const nextIndex = txt.indexOf("hey" ,currentIndex)
+  console.log(`nextIndex: ${nextIndex}`)
+
+  if(nextIndex >= 0){
+    count ++;
+    currentIndex = nextIndex + 1
+  }else {
+    break
+  }
+}
+console.log(`count: ${count}`)
