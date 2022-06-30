aae727f022f0775bf201aa2fe484c04e40d1ebf6
nsk0826 <nsk.20160401@gmail.com>
2022-07-01 01:35:43 +0900

    format

--- src/numberToString.ts
+++ src/numberToString.ts
@@ -1,11 +1,11 @@
-export {}
+export {};
 
 type Human = {
-  age:number,
-  name: string
-}
+  age: number;
+  name: string;
+};
 
 type HumanString = {
-  age:string,
-  name:string
-}
+  age: string;
+  name: string;
+};
